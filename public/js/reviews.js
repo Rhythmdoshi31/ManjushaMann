82dbd8a69ea408ca132603ad5a0034a940baf39b
Rhythmdoshi31 <rhythmdoshi04@gmail.com>
2025-04-27 15:04:25 +0530

    Fix: reel pop-up overflow

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -1,13 +1,15 @@
 const paras = [
     "Thank you so much ma'am for your insightful tarot reading... Your guidance came at a time when I truly needed it and has brought me much clarity... I'm already seeing positive changes in my life thanks to your suggestions. Grateful for your wisdom and kindness.",
     "Your guidance gave me clarity and a deeper underdtanding of my current situation. I am truly grateful for your calm and compassionate approach, which made me feel at ease throughout the session. I highly recommend your services to anyone seeking direction and peace of mind.",
-    "Aapse milkar achcha laga aap ki di gayi jankari aur upay bade hia acche aur kargar hai dhanyawad❤️"
+    "Aapse milkar achcha laga aap ki di gayi jankari aur upay bade hia acche aur kargar hai dhanyawad❤️",
+    "आपने मेरे बारे में जो बताया है वो सच है।और मेरे परिवार वालों के बारे में जो भी बताए सही है। आपने जो उपचार बताएं है उसका पालन कर रहा हूँ। उससे मेरे जीवन में सुंदर हुआ है। बहुत बहुत आभार और धन्यवाद आपको। बहुत अच्छी knowledge है आपको एस्ट्रोलॉजी के बारे में।"
 ];
 
 const names = [
     {name: "Divya Mundaliar", place: "Rawan", imgSrc: "./images/reviewer.webp"},
     {name: "mr.rashal94", place: "Gwalior", imgSrc: "./images/reviewer2.webp"},
     {name: "Rabia Basree", place: "Rawan", imgSrc: "./images/reviewer3.webp"},
+    {name: "P Naresh Raju", place: "Raipur", imgSrc: "./images/unknown.webp"},
 ];
 
 let index = 0;
